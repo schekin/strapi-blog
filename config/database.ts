876e19788ce4aa25eaa57bcef7b540d6ed7c4cb0
Pavel Shchokin <p.shchokin@allright.com>
2023-07-02 21:54:34 +0300

    fix(cicd): update

--- config/database.ts
+++ config/database.ts
@@ -7,7 +7,7 @@ export default ({ env }) => ({
       database: env('DATABASE_NAME'),
       user: env('DATABASE_USERNAME'),
       password: env('DATABASE_PASSWORD'),
-      ssl: true,
+      "ssl": { "rejectUnauthorized": false }
     },
   },
 });
